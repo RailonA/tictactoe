fe596b96addadde0ff9e7f45975a18931656660d
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-01-19 18:03:04 -0300

    Add more cases for PlayerInfo #initialize

--- spec/game_logic_spec.rb
+++ spec/game_logic_spec.rb
@@ -169,10 +169,18 @@ end
 describe PlayerInfo do
   let(:initial_values1) { { name: '', symbol: 'X', score: 0 } }
   let(:initial_values2) { { name: '', symbol: 'O', score: 0 } }
+  let(:test_player) { Player.new }
   describe '#initialize' do
     it 'checks that both players have their corresponding parameters' do
       expect(initial_values1).to eql(PlayerInfo.new.player[0])
       expect(initial_values2).to eql(PlayerInfo.new.player[1])
+    end
+    it 'checks that players are not the same' do
+      expect(test_player.player.player[0]).not_to eql(test_player.player.player[1])
+    end
+    it 'checks that player scores are 0' do
+      expect(test_player.player.player[0][:score]).to eql(0)
+      expect(test_player.player.player[1][:score]).to eql(0)
     end
   end
 end
